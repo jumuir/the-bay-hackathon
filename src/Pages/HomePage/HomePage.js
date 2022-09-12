8dca0c0bfbbd207d48e8ab5a4174f047b6294f9d
jumuir <justinmuir@gmail.com>
2022-09-12 17:25:38 -0400

    hero added to landing page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './HomePage.scss';
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+  return (<>
+    <section className='home__hero'>
+      <div className='home__hero-overlay'></div>
+      <h1 className='home__hero-title'>Father's Day</h1>
+      <h1 className='home__hero-title'>SALES</h1>
+    </section>
+    <section className='home__categories'>
+      
+    </section>
+  </>)
 }
 
 export default HomePage

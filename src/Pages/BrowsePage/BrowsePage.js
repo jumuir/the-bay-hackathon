c124b58ba7805664315c47174b209c9ce1286c6d
jumuir <justinmuir@gmail.com>
2022-09-13 10:04:13 -0400

    styling browse page

--- src/Pages/BrowsePage/BrowsePage.js
+++ src/Pages/BrowsePage/BrowsePage.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import './BrowsePage.scss';
 
 const BrowsePage = () => {
-  return (
-    <div>BrowsePage</div>
-  )
+  return (<>
+    <section className='browse__hero'>
+      <div className='browse__hero-overlay'></div>
+      <h1 className='browse__hero-title'>Up to 60% off</h1>
+      <h1 className='browse__hero-title'>Dresses</h1>
+    </section>
+    <section className='browse__top'>
+      <div className='browse__path-container'>
+        <p className='browse__path'><span className='hover-line'>Home</span> / <span className='hover-line'>Women</span> / <span className='hover-line'>Women's Clothing</span> / <span className='hover-line'>Dresses</span></p>
+      </div>
+      <div className='browse__title-container'>
+        <h2 className='browse__title'>Dresses</h2>
+        <h3 className='browse__count'>5,674 Items</h3>
+      </div>
+      <div className='browse__category-filters'>
+        <button className='browse__category-filter browse__category-filter--active'>Style</button>
+        <button className='browse__category-filter'>Occasion</button>
+      </div>
+      <div className='browse__categories'>
+        <div className='browse__category-card'>
+          <div className='browse__category-card-image browse__category-card-image--1'></div>
+          <h3 className='browse__category-card-title'>Knit</h3>
+        </div>
+        <div className='browse__category-card'>
+          <div className='browse__category-card-image browse__category-card-image--2'></div>
+          <h3 className='browse__category-card-title'>Pleated</h3>
+        </div>
+        <div className='browse__category-card'>
+          <div className='browse__category-card-image browse__category-card-image--3'></div>
+          <h3 className='browse__category-card-title'>Retro Boho</h3>
+        </div>
+        <div className='browse__category-card'>
+          <div className='browse__category-card-image browse__category-card-image--4'></div>
+          <h3 className='browse__category-card-title'>Party</h3>
+        </div>
+        <div className='browse__category-card'>
+          <div className='browse__category-card-image browse__category-card-image--5'></div>
+          <h3 className='browse__category-card-title'>Cut-out</h3>
+        </div>
+        <div className='browse__category-card'>
+          <div className='browse__category-card-image browse__category-card-image--6'></div>
+          <h3 className='browse__category-card-title'>Mini</h3>
+        </div>
+      </div>
+    </section>
+    <section className='browse__body'>
+      <div className='browse__body-filters'>
+        <div className='browse__body-filters-search-bar'>
+          <select className='browse__body-filters-search'>
+            <option value="" selected>Sort by Featured</option>
+          </select>
+        </div>
+        
+      </div>
+      <div className='browse__body-items'>
+        
+      </div>
+    </section>
+  </>)
 }
 
 export default BrowsePage

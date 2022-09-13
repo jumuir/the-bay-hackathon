38ef5eae06d9a7dfa8f8125dbe26d3c42ec94a5d
jumuir <justinmuir@gmail.com>
2022-09-13 13:03:43 -0400

    fixed image bg, styling filter bar

--- src/Pages/BrowsePage/BrowsePage.js
+++ src/Pages/BrowsePage/BrowsePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BrowsePage.scss';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const BrowsePage = () => {
   return (<>
@@ -50,14 +51,79 @@ const BrowsePage = () => {
     <section className='browse__body'>
       <div className='browse__body-filters'>
         <div className='browse__body-filters-search-bar'>
-          <select className='browse__body-filters-search'>
-            <option value="" selected>Sort by Featured</option>
+          <select defaultValue="" className='browse__body-filters-search'>
+            <option value="">Sort by Featured</option>
           </select>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Category</h3>
+            <AiOutlineMinus />
+          </div>
+          <div className='browse__filter-subheader'></div>
+          <h4 className='browse__filter-subheader hover-line'>Women's Clothing</h4>
+          <h4 className='browse__filter-subheader bold hover-line'>Dresses</h4>
+          <p className='browse__filter-type hover-line'>Bridesmaid Dresses</p>
+          <p className='browse__filter-type hover-line'>Casual Dresses</p>
+          <p className='browse__filter-type hover-line'>Cocktail & Party Dresses</p>
+          <p className='browse__filter-type hover-line'>Formal Gowns</p>
+          <p className='browse__filter-type hover-line'>Little Black Dresses</p>
+          <p className='browse__filter-type hover-line'>Maternity Dresses</p>
+          <p className='browse__filter-type hover-line'>Mother of the Bride Dresses</p>
+          <h4 className='browse__filter-subheader underline hover-line'>View More</h4>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Brand</h3>
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Price</h3>
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Size Range</h3>
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Size</h3>
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Colour</h3>
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Style</h3>
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Responsible</h3>
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div className='browse__filter-container'>
+          <div className='browse__filter-header-container'>
+            <h3 className='browse__filter-header'>Neckline</h3>
+            <AiOutlinePlus />
+          </div>
         </div>
         
       </div>
-      <div className='browse__body-items'>
         
+      <div className='browse__body-items'>  
       </div>
     </section>
   </>)
